5f84177580e2da1a173adb17265f579c6bc302f1
nicholascloud <me@nicholascloud.com>
2013-03-31 12:42:52 -0500

    fixed problem with archive

--- tasks/archive-tasks.js
+++ tasks/archive-tasks.js
@@ -6,10 +6,13 @@ var config = require('./config'),
   exec = require('child_process').exec;
 
 var COMPRESS_CMD = "tar --exclude='.DS_Store' -cvzf " +
-  config.ARCHIVE_DEST_PATH + " " +
-  config.ARCHIVE_SRC_PATH;
+  config.ARCHIVE_DEST_PATH + " -C " +
+  config.BUILD_DIR + " " +
+  config.DOCSET_NAME;
 
 task('archive', {async: true}, function () {
+  console.log('Archiving docset...');
+  console.log('  >', COMPRESS_CMD);
   exec(COMPRESS_CMD, function (err) {
     if (err) {
       console.error(err);
